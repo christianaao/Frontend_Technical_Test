a63bca6f2857f75e987c41ad60162135936232c9
Olamide Olawuyi <christianaao@ChristianaO-Laptop>
2025-02-02 23:41:24 +0000

    added edit and delete functionality to landing-page component

--- app/components/landing-page/component.js
+++ app/components/landing-page/component.js
@@ -1,5 +1,11 @@
 import { action } from '@ember/object';
-import { collection, getFirestore, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  getFirestore,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+} from 'firebase/firestore';
 import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
 import podNames from 'ember-component-css/pod-names';
@@ -20,7 +26,17 @@ export default class LandingPage extends Component {
    *   await deleteDoc(movie.ref);
    *
    */
+
   @tracked movies;
+
+  @tracked editingMovie = null
+  @tracked editFormData = {
+    title: "",
+    description: "",
+    rating: "",
+    genre: "",
+    releaseYear:""
+  }
 
   @action async loadMovies() {
     const db = getFirestore();
@@ -32,10 +48,49 @@ export default class LandingPage extends Component {
 
     this.movies = movies;
   }
+  // when user clicks "Edit" button, this function will be called and store data
+  editForm(movie) {
+    this.editForm = movie
+    this.editFormData = {
+      title: movie.data().title,
+      description: movie.data().description,
+      rating: movie.data().rating,
+      genre: movie.data().genre,
+      releaseYear: movie.data().releaseYear
+    }
+  }
+
+  // when user clicks "Submit" button, movie will update with data stored in editFormData
+  @action async submitEditedMovie(event) {
+    event.preventDefault()
+    if(!this.editForm) {return;}
+    await updateDoc(this.editForm.ref, {
+      title: this.editFormData.title,
+      description: this.editFormData.description,
+      rating: this.editFormData.rating,
+      genre: this.editFormData.genre,
+      releaseYear: this.editFormData.releaseYear
+    });
+
+    alert("Movie successfully updated.")
+    this.editForm = null
+    this.loadMovies()
+  }
+
+  @action cancelEdit() {
+    this.editForm = null
+  }
+ 
+  async deleteMovie(movie) {
+    if (confirm('Are you sure you want to permanently delete this movie?')) {
+      await deleteDoc(movie.ref);
+      alert('Movie successfully deleted.');
+      this.loadMovies();
+    }
+  }
 
   constructor(owner, args) {
     super(owner, args);
-
     this.loadMovies();
   }
 }
--- app/components/movie-list/movie-list-item/component.js
+++ app/components/movie-list/movie-list-item/component.js
@@ -5,7 +5,7 @@ export default class MovieListItem extends Component {
   styleNamespace = podNames['movie-list/movie-list-item'];
 
   get movie() {
-    console.log(this.args.movie.data());
+    console.log("MOVIE LIST ITEM: ", this.args.movie.data());
     return this.args.movie.data();
   }
 }
